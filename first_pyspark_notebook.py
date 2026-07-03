e20faca65e85d597859d3e8552f714239f2f3220
Anna Grigoriadi <AnnaGrigoriadiG@gmail.com>
2026-07-03 14:44:31 +0100

    Delete first_pyspark_notebook.py

--- first_pyspark_notebook.py
+++
@@ -1,9 +0,0 @@
-
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("FirstNotebook").getOrCreate()
-
-data = [("Anna", 25), ("John", 30)]
-df = spark.createDataFrame(data, ["Name", "Age"])
-
-df.show()
